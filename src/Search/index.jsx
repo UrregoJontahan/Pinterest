6432d064d614052d2d1cc710bccd0ded3861e70c
jonathan <jdavid598@hotmail.com>
2024-03-14 09:29:14 -0500

    agrego evento de click y x en la barra de busqueda

--- src/Search/index.jsx
+++ src/Search/index.jsx
@@ -1,14 +1,40 @@
-import React from "react";
-import "./search.css"
+import React, { useState, useRef, useEffect } from "react";
+import "./search.css";
 
-export function Search () {
-    return (
-        <div className="counter-input">
-            <input 
-            type="search" 
-            className="btn-search"
-            placeholder="🔍 Buscar"
-            />
-        </div>
-    )
+export function Search() {
+  const [clickInX, setClickInX] = useState(false);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (
+        inputRef.current &&
+        !inputRef.current.contains(e.target) &&
+        !e.target.classList.contains("btn-x-cancel")
+      ) {
+        setClickInX(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="counter-input">
+      <input
+        type="search"
+        className="btn-search"
+        placeholder="🔍 Buscar"
+        onClick={() => setClickInX(true)}
+        ref={inputRef} 
+      />
+      <div className="container-x-cancel">
+        {clickInX && <button className="btn-x-cancel">x</button>}
+      </div>
+    </div>
+  );
 }
