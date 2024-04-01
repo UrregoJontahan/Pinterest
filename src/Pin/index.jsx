0f8a9214c07d2ca1d905a7a4b8d341e316f3b136
jonathan <jdavid598@hotmail.com>
2024-04-01 09:30:47 -0500

    push de nuevo

--- src/Pin/index.jsx
+++ src/Pin/index.jsx
@@ -11,7 +11,6 @@ export function Pin() {
   const { categoriesMap }  = useContext(DataContext);
   const { id: currentId } = useParams(); 
   const [selectedImage, setSelectedImage] = useState(null);
-
   const [showConfeti, setShowConfeti] = useState(false);
 
   const handleLikeClick = () => {
@@ -31,6 +30,71 @@ export function Pin() {
       setSelectedImage(result);
   };
 
+  const uniqueImagesMap = {};
+  for (const [_, images] of categoriesMap) {
+    images.forEach((image) => {
+      if (!uniqueImagesMap.hasOwnProperty(image._id)) {
+        uniqueImagesMap[image._id] = image;
+      }
+    });
+  }
+
+  const uniqueImages = Object.values(uniqueImagesMap);
+
+  return (
+    <div className="container-images-selecte-suggestions">
+      <div className="page-pin">
+        <ButtonHomePage />
+        <div className="details-image">
+          {selectedImage && selectedImage.image && (
+            <img
+              className="image-selected"
+              src={selectedImage.image}
+              alt="Selected Image"
+            />
+          )}
+          <div className="box-comments">
+            <div className="btn-save-image-selected" onClick={handleLikeClick}> Guardar
+              {showConfeti && <Confeti/>}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="suggestion-images">
+        <div className="image-columns">
+          {uniqueImages.map(({ image, _id }) => (
+            <div key={_id} className="image-container">
+              <Link to={`/page-pin/${_id}`}>
+                <img src={image} className="img-ramdom" />
+              </Link>
+              <button className="btn-save-image" onClick={handleLikeClick}>
+                <FaHeart className="icon-heart" />
+                {showConfeti && <Confeti />}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );  
+}
+
+// {allImages.map(({image, _id}) => (
+//   <div className="image-container">
+//     <Link 
+//     to={`/page-pin/${_id}`}
+//     key={_id}  >
+//     <img
+//       src={image}
+//       className="img-ramdom"
+//     />
+//   </Link>
+//     <button className="btn-save-image" onClick={handleLikeClick}>
+//     <FaHeart className="icon-heart" />
+//       {showConfeti && <Confeti/>}
+//     </button>
+//   </div>
+// ))}
 
 // const categoriesMap = new Map();
 
@@ -87,64 +151,3 @@ export function Pin() {
   //   })
     
   // let allImages= imagesUniques
-  
-  return (
-    <div className="container-images-selecte-suggestions">
-      <div className="page-pin">
-        <ButtonHomePage />
-        <div className="details-image">
-          {selectedImage && selectedImage.image && (
-            <img
-              className="image-selected"
-              src={selectedImage.image}
-              alt="Selected Image"
-            />
-          )}
-          <div className="box-comments">
-            <div className="btn-save-image-selected" onClick={handleLikeClick}> Guardar
-              {showConfeti && <Confeti/>}
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="suggestion-images">
-        <div className="image-columns">
-        {Array.from(categoriesMap).map(([category, images]) => (
-  <div key={category}>
-    <h2>{category}</h2>
-    {images.map(({ image, _id }) => (
-      <div key={_id} className="image-container">
-        <Link to={`/page-pin/${_id}`}>
-          <img src={image} className="img-ramdom" />
-        </Link>
-        <button className="btn-save-image" onClick={handleLikeClick}>
-          <FaHeart className="icon-heart" />
-          {showConfeti && <Confeti />}
-        </button>
-      </div>
-    ))}
-  </div>
-))}
-
-        </div>
-      </div>
-    </div>
-  );
-}
-
-// {allImages.map(({image, _id}) => (
-//   <div className="image-container">
-//     <Link 
-//     to={`/page-pin/${_id}`}
-//     key={_id}  >
-//     <img
-//       src={image}
-//       className="img-ramdom"
-//     />
-//   </Link>
-//     <button className="btn-save-image" onClick={handleLikeClick}>
-//     <FaHeart className="icon-heart" />
-//       {showConfeti && <Confeti/>}
-//     </button>
-//   </div>
-// ))}
