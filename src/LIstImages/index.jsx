75ac8e49c8439a07c863690a8a5ca52b8ead758e
jonathan <jdavid598@hotmail.com>
2024-04-05 16:28:21 -0500

    push

--- src/LIstImages/index.jsx
+++ src/LIstImages/index.jsx
@@ -1,71 +1,77 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import "./listImages.css";
 import { Link } from "react-router-dom";
 import { DataContext } from "../Provider";
 import { FaHeart } from "react-icons/fa6";
 import { Confeti } from "../congratulations";
-import loading from "../assets/loading.jpg"
-import useObserver from "../UseObserver";
 
 export function ListImages() {
-    const { filteredImages } = useContext(DataContext)
+    const { filteredImages } = useContext(DataContext);
     const [showConfeti, setShowConfeti] = useState(false);
-    const [isLoading, setLoadingState] = useState("hidden")
+    const [isLoading, setLoadingState] = useState(true);
+    const [show, setShow] = useState(false);
+    const containerRef = useRef(null);
+    const [showAdditionalImages, setShowAdditionalImages] = useState(false);
+
 
     const handleLikeClick = () => {
         setShowConfeti(true);
         setTimeout(() => {
             setShowConfeti(false);
-        }, 1000); 
+        }, 1000);
     };
 
-    const [observer, setElements, entries] = useObserver({
-      threshold:0.25,
-      root:null
-    });
+    useEffect(() => {
+        const handleIntersect = (entries, observer) => {
+            const el = entries[0];
+            console.log(el.isIntersecting);
+            if (el.isIntersecting) {
+                setShow(true);
+                observer.disconnect();
+            }
+        };
 
-    useEffect(()=>{
-      const images = document.querySelectorAll(".image-container");
-      setElements(images)
-    },[setElements]);
+        const observer = new IntersectionObserver(handleIntersect, {
+            root: null,
+            rootMargin: "0px",
+            threshold: 0.7
+        });
 
-    useEffect(()=>{
-      entries.forEach(entry=> {
-        if(entry.isIntersecting){
-          const lazyImage = entry.target;
-          lazyImage.src = lazyImage.getAttribute('data-src');
-          lazyImage.classLIst.remove("img-ramdom");
-          observer.unobserve(lazyImage) 
-        }
-      });
-    },[entries,observer])
+        observer.observe(containerRef.current);
 
-  return (
-    <div className="father-counter-images">
-      <div className="counter-images">
-        <div className="images">
-          {filteredImages.map(({ image, _id }) => (
-            <div key={_id + Date.now() + Math.random()} className="image-container" >
-                <Link to={`/page-pin/${_id}`}>
-                {isLoading && <div className="loader"/>}
-                  <img 
-                    // loading="lazy" 
-                    // decoding="async"
-                    className={`img-ramdom ${isLoading && "loading-opacity"}`}
-                    onLoad={()=> setLoadingState("")}
-                    src={loading} 
-                    data-src={image}
-                    key={image}
-                  />
-                </Link>
-              <button className="btn-save-image" onClick={handleLikeClick}>
-                <FaHeart className="icon-heart" />
-                   {showConfeti && <Confeti />}
-              </button>
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
+
+    return (
+        <div className="father-counter-images">
+            <div className="counter-images" ref={containerRef}>
+                {show && (
+                    <div className="images">
+                        {filteredImages.map(({ image, _id }) => (
+                            <div key={_id + Date.now() + Math.random()} className="image-container">
+                                <Link to={`/page-pin/${_id}`}>
+                                    {isLoading && <div className="countainer-loader"><div className="loader" /></div>}
+                                    <img
+                                        loading="lazy"
+                                        decoding="async"
+                                        className={`img-ramdom ${isLoading && "loading-opacity"}`}
+                                        src={image}
+                                        onLoad={() => setLoadingState(false)}
+                                    />
+                                </Link>
+                                <button className="btn-save-image" onClick={handleLikeClick}>
+                                    <FaHeart className="icon-heart" />
+                                    {showConfeti && <Confeti />}
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
-          ))}
         </div>
-      </div>         
-    </div>
-  );
+    );
 }
+
+
